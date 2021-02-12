9f59dcd726f64a27bcdbf2e118281b214d6ae1c0
Mackenzie Sharp <mackenziesharp5@gmail.com>
2021-02-11 23:39:41 -0500

    Removed global variables

--- nqueens.py
+++ nqueens.py
@@ -3,81 +3,73 @@ import random
 
 # Implement a solver that returns a list of queen's locations
 #  - Make sure the list is the right length, and uses the numbers from 0 .. BOARD_SIZE-1
-rows = list()
-cols = list() # Row index for each queen
-diag1 = list() # Goes from bottom left to top right
-diag2 = list() # Goes from bottom right to top left
     
 def solve(board_size):
+    data = {'size': board_size, 'rows': [], 'cols': [], 'diag1': [], 'diag2': []}
+
     moved = False
 
     while True:
         if not moved:
-            setup(board_size)
+            setup(data)
         
         moved = False
-        most_collisions = calculate_most_collisions(board_size)
+        most_collisions = calculate_most_collisions(data)
 
         if len(most_collisions) == 0:
-            return format_solution()
+            return format_solution(data)
 
         for col in most_collisions:
-            row = optimize_column(board_size, col)
+            row = optimize_column(data, col)
 
             if row is None:
                 continue
 
-            move(board_size, col, row)
+            move(data, col, row)
             moved = True
             break
 
-def move(board_size, col, new_row):
-    global rows, cols, diag1, diag2
-
-    row = cols[col]
+def move(data, col, new_row):
+    row = data['cols'][col]
 
     if row == new_row:
         return
 
-    rows[row] -= 1
-    diag1[calculate_diag(col, row)] -= 1
-    diag2[calculate_diag(col, row, board_size)] -= 1
+    data['rows'][row] -= 1
+    data['diag1'][calculate_diag(col, row)] -= 1
+    data['diag2'][calculate_diag(col, row, data['size'])] -= 1
 
     row = new_row
-    cols[col] = row
-    rows[row] += 1
-    diag1[calculate_diag(col, row)] += 1
-    diag2[calculate_diag(col, row, board_size)] += 1
+    data['cols'][col] = row
+    data['rows'][row] += 1
+    data['diag1'][calculate_diag(col, row)] += 1
+    data['diag2'][calculate_diag(col, row, data['size'])] += 1
 
-def calculate_collisions(board_size, col, row=None):
-    global rows, cols, diag1, diag2
-
+def calculate_collisions(data, col, row=None):
     total = -3
 
     if row is None:
-        row = cols[col]
+        row = data['cols'][col]
     else:
         total += 3
     
     diag1_index = calculate_diag(col, row)
-    diag2_index = calculate_diag(col, row, board_size)
+    diag2_index = calculate_diag(col, row, data['size'])
 
-    total += rows[row] + diag1[diag1_index] + diag2[diag2_index]
+    total += data['rows'][row] + data['diag1'][diag1_index] + data['diag2'][diag2_index]
 
     return total
 
-def optimize_column(board_size, col):
-    global rows, cols
-
-    current_row = cols[col]
-    min_collisions = calculate_collisions(board_size, col)
+def optimize_column(data, col):
+    current_row = data['cols'][col]
+    min_collisions = calculate_collisions(data, col)
     result = None
 
-    for row in range(len(rows)):
+    for row in range(len(data['rows'])):
         if row == current_row:
             continue
 
-        collisions = calculate_collisions(board_size, col, row)
+        collisions = calculate_collisions(data, col, row)
 
         if collisions >= min_collisions:
             continue
@@ -87,19 +79,15 @@ def optimize_column(board_size, col):
 
     return result
 
-def format_solution():
-    global cols
+def format_solution(data):
+    return [i + 1 for i in data['cols']]
 
-    return [i + 1 for i in cols]
-
-def calculate_most_collisions(board_size):
-    global cols
-
+def calculate_most_collisions(data):
     max_collisions = 1
     collisions = []
 
-    for col in range(len(cols)):
-        total_collisions = calculate_collisions(board_size, col)
+    for col in range(len(data['cols'])):
+        total_collisions = calculate_collisions(data, col)
 
         if total_collisions < max_collisions:
             continue
@@ -112,33 +100,28 @@ def calculate_most_collisions(board_size):
     
     return collisions
 
-def setup(board_size):
-    global rows, cols, diag1, diag2
+def setup(data):
+    n = data['size']
 
-    rows = [0] * board_size
-    cols = [0] * board_size
-    diag1 = [0] * (board_size * 2 - 1)
-    diag2 = [0] * (board_size * 2 - 1)
+    data['rows'] = [0] * n
+    data['cols'] = [0] * n
+    data['diag1'] = [0] * (n * 2 - 1)
+    data['diag2'] = [0] * (n * 2 - 1)
 
-    randomize(board_size)
+    for col in range(n):
+        row = random.randint(0, n - 1)
+        data['rows'][row] += 1
+        data['cols'][col] = row
+        data['diag1'][calculate_diag(col, row, data['size'])] += 1
+        data['diag2'][calculate_diag(col, row, data['size'])] += 1
 
-def calculate_diag(col, row, board_size=None):
-    if board_size is None:
+def calculate_diag(col, row, size=None):
+    if size is None:
         # diag 1
         return col + row
-    
-    # diag 2
-    return board_size - 1 - col + row
-
-def randomize(board_size):
-    global rows, cols, diag1, diag2
-
-    for col in range(board_size):
-        row = random.randint(0, board_size - 1)
-        rows[row] += 1
-        cols[col] = row
-        diag1[calculate_diag(col, row)] += 1
-        diag2[calculate_diag(col, row, board_size)] += 1
+    else:
+        # diag 2
+        return size - 1 - col + row
 
 start_time = time.time()
 print(solve(1000))
